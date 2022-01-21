37c6403bc1d8cf35b0fb27c060a66bd4f6108d00
Andrey Malafeyev <malafeev7@bk.ru>
2022-01-21 13:28:17 +0600

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,7 @@ import program from "commander";
 
 program
   .description("Compares two configuration files and shows a difference.")
-  .version("0.1.0", "-V, --version", "output the version number");
+  .version("0.1.0", "-V, --version", "output the version number")
+  .option("-f, --format [type]", "Output format");
 
 program.parse(process.argv);
