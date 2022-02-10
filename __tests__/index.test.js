e11bff1d5c741841152060a4fe028f1e97585dc2
Andrey Malafeyev <malafeev7@bk.ru>
2022-02-10 13:59:08 +0600

    new

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,21 +1,22 @@
-import { test, expect } from '@jest/globals';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
-import { readFileSync } from 'fs';
-import genDiff from '../src/index.js';
+import { test, expect } from "@jest/globals";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import { readFileSync } from "fs";
+import genDiff from "../src/index.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
-const pathFile1 = getFixturePath('file1.json');
-const pathFile2 = getFixturePath('file2.json');
-const pathFileOutput = getFixturePath('changes');
+const getFixturePath = (filename) =>
+  path.join(__dirname, "..", "__fixtures__", filename);
+const pathFile1 = getFixturePath("file1.json");
+const pathFile2 = getFixturePath("file2.json");
+const pathFileOutput = getFixturePath("changes.txt");
 const dataFileOutput = readFileSync(pathFileOutput, {
-  encoding: 'utf8',
-  flag: 'r',
+  encoding: "utf8",
+  flag: "r",
 });
 
-test('gendif flat file', () => {
+test("gendif flat file", () => {
   expect(genDiff(pathFile1, pathFile2)).toBe(dataFileOutput);
 });
